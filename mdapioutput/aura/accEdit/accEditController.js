46b537ddb9a06d8f259c80e239a08fad41b1a68b
akhilesh B <akkibaddi@outlook.com>
2017-11-08 15:53:57 -0500

    finished LDS badge

--- mdapioutput/aura/accEdit/accEditController.js
+++ mdapioutput/aura/accEdit/accEditController.js
@@ -1,13 +1,20 @@
 ({
-    handleSaveRecord : function(component, event, helper) {
-        component.find("recordEdit").saveRecord($A.getCallback(function(saveResult){
+    handleSaveRecord : function(cmp, event, helper) {
+        cmp.find("recordEdit").saveRecord($A.getCallback(function(saveResult){
             if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
                 console.log("Save completed successfully.");
+                
+
             } else if (saveResult.state === "INCOMPLETE") {
                 console.log("User is offline, device doesn't support drafts.");
             } else if (saveResult.state === "ERROR") {
                 console.log('Problem saving record, error: ' + 
                            JSON.stringify(saveResult.error));
+                           var errMsg="";
+                           for(var i=0;i<saveResult.error.length;i++){
+                               errMsg+=saveResult.error[i].message+"\n";
+                           }
+                           cmp.set("v.recordSaveError",errMsg);
             } else {
                 console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
             }
